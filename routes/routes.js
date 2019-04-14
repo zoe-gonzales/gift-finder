0a095ada84c7f36bd7a2c613dc04a32ea78f9f0b
zoe-gonzales <zoegonzales03@gmail.com>
2019-04-14 16:41:46 -0600

    styling of form pages

--- routes/routes.js
+++ routes/routes.js
@@ -10,4 +10,17 @@ module.exports = function(app) {
             res.render('survey', {categories: data});
         });
     });
+
+    app.get('/add', function(req, res){
+        db.Idea.findAll({}).then(function(data){
+            var categories = [];
+            data.forEach(category => {
+                var categoryName = category.dataValues.category;
+                if (!categories.includes(categoryName)){
+                    categories.push(categoryName);
+                }
+            });
+            res.render('add', {category: categories});
+        });
+    });
 }
