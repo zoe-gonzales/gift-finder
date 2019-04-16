677ef245ec22e0ef8c024007b7c800d3e17ab213
zoe-gonzales <zoegonzales03@gmail.com>
2019-04-15 19:03:05 -0600

    writing sql queries to insert to deployed db

---
+++ models/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO Ideas (gift_name, category, isHomemade, price)
+VALUES 
+('Headphones', 'Tech', false, '21-50'),
+('Camelbak', 'Outdoors', false, '51-100'),
+('Coffee Subscription', 'Food & Dining', false, '51-100'),
+('Handmade scarf', 'Fashion', true, '21-50'),
+('FitBit', 'Health & Fitness', false, '51-100'),
+('Band t-shirt', 'Music', false, '21-50'),
+('Painting class', 'Art', false, '21-50'),
+('Dog sweater', 'Pets', false, '21-50'),
+('Duffel bag', 'Travel', false, '51-100'),
+('Netflix Subscription for 6 months', 'TV', false, '51-100'),
+('Face mask', 'Beauty & Skincare', false, '0-20');
